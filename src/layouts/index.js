c4abd676705ac0299241e91e41e0c7f6c773e84a
Xerik <dsshar@gmail.com>
2018-04-09 09:32:01 -0400

    fixed issued with appbar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,32 +1,30 @@
 import React from 'react'
+import { withStyles, MuiThemeProvider } from 'material-ui/styles'
 import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import AppBar from './AppBar'
+import theme from '../misc/fusTheme'
 
-import Header from '../components/Header'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+const TemplateWrapper = ({ classes, children }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <Helmet
+          title="Gatsby Default Starter"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        />
+        <AppBar />
+        {children()}
+      </div>
+    </MuiThemeProvider>
+  )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
